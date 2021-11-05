8b733846b63339d76ddb0e01d72e4d6baf816cd3
Heather King <hkingdevelopment@gmail.com>
2021-11-05 17:04:33 +0000

    adds tests for lesson class
    initial tests just for the class attributes

---
+++ tests/lesson_test.py
@@ -0,0 +1,27 @@
+import unittest
+
+from models.lesson import Lesson
+
+class TestLesson(unittest.TestCase):
+
+    def setUp(self):
+        self.lesson1 = Lesson("Beginner Recorder", "Billy Bob", "27-11-2021", "Recorder", True)
+        self.lesson2 = Lesson("Piano 1:1", "Jane Doe", "27-11-2021", "Piano")
+
+    def test_lesson_has_name(self):
+        self.assertEqual("Beginner Recorder", self.lesson1.name)
+
+    def test_lesson_has_tutor(self):
+        self.assertEqual("Billy Bob", self.lesson1.tutor)
+    
+    def test_lesson_has_date(self):
+        self.assertEqual("27-11-2021", self.lesson1.date)
+    
+    def test_lesson_has_instrument(self):
+        self.assertEqual("Recorder", self.lesson1.instrument)
+    
+    def test_lesson_has_group_status(self):
+        self.assertEqual(True, self.lesson1.group)
+
+    def test_lesson_has_id(self):
+        self.assertEqual(None, self.lesson1.id)
