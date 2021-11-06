85b2fa354cddf82a96f6f29943bd1dee78bafded
Heather King <hkingdevelopment@gmail.com>
2021-11-06 15:36:22 +0000

    adds pupils route to pupil_controller

--- controllers/pupil_controller.py
+++ controllers/pupil_controller.py
@@ -6,5 +6,8 @@ import repositories.pupil_repository as pupil_repository
 pupils_blueprint = Blueprint("pupils", __name__)
 
 
-# @pupils_blueprint.route("/pupils")
-# def pupils():
+@pupils_blueprint.route("/pupils")
+def pupils():
+    pupils = pupil_repository.select()
+    return render_template("pupils/index.html", all_pupils=pupils)
+    
