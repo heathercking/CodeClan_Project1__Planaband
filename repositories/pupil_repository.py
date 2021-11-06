6aa870bf60ccf66c836912a89bf49a99169c5d4d
Heather King <hkingdevelopment@gmail.com>
2021-11-06 15:19:37 +0000

    adds save method to pupil repository

--- repositories/pupil_repository.py
+++ repositories/pupil_repository.py
@@ -1,5 +1,13 @@
+from re import S
 from db.run_sql import run_sql
 
 from models.pupil import Pupil
 import repositories.pupil_repository as pupil_repository
+
+def save(pupil):
+    sql = "INSERT INTO pupils (name, dob, instrument, grade, notes, nok_id) VALUES (%s, %s, %s, %s, %s, %s)"
+    values = [pupil.name, pupil.dob, pupil.instrument, pupil.grade, pupil.notes, pupil.nok.id]
+    results = run_sql(sql, values)
+    pupil.id = results[0]['id']
+    return pupil
 
