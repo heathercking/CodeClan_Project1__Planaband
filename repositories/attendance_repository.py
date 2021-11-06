7402ea6b36c6a5734a8a2299f8dd917a879f698f
Heather King <hkingdevelopment@gmail.com>
2021-11-06 19:44:19 +0000

    adds all methods to attendance_repository

--- repositories/attendance_repository.py
+++ repositories/attendance_repository.py
@@ -1,5 +1,6 @@
 from db.run_sql import run_sql
 
+from models.attendance import Attendance
 from models.lesson import Lesson
 from models.pupil import Pupil
 import repositories.lesson_repository as lesson_repository
@@ -13,3 +14,47 @@ def save(attendance):
     id = results[0]['id']
     attendance.id = id
     return attendance
+
+
+def select_all():
+    attendances = []
+
+    sql = "SELECT * FROM attendances"
+    results = run_sql(sql)
+
+    for row in results:
+        lesson = lesson_repository.select(row['lesson_id'])
+        pupil = pupil_repository.select(row['pupil_id'])
+        attendance = Attendance(lesson, pupil, row['attended'], row['id'])
+        attendances.append(attendance)
+    return attendances
+
+
+def select(id):
+    attendance = None
+    sql = "SELECT * FROM attendances WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        lesson = lesson_repository.select(result['lesson_id'])
+        pupil = pupil_repository.select(result['pupil_id'])
+        attendance = Attendance(lesson, pupil, result['attended'], result['id'])
+    return attendance
+
+
+def update(attendance):
+    sql = "UPDATE attendances SET (lesson_id, pupil_id, attended) = (%s, %s, %s) WHERE id = %s"
+    values = [attendance.lesson.id, attendance.pupil.id, attendance.attended, attendance.id]
+    run_sql(sql, values)
+
+
+def delete(id):
+    sql = "DELETE FROM attendances WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
+def delete_all():
+    sql = "DELETE FROM attendances"
+    run_sql(sql)
