aa649125d639e55cee46d7ffb22967d752913c8d
Heather King <hkingdevelopment@gmail.com>
2021-11-06 14:23:11 +0000

    adds save function to nok repository

--- repositories/nok_repository.py
+++ repositories/nok_repository.py
@@ -4,4 +4,10 @@ from models.nok import NextOfKin
 import repositories.nok_repository as nok_repository
 
 
+def save(nok):
+    sql = "INSERT INTO noks (name, contact_number, address, postcode) VALUES (%s, %s, %s, %s) RETURNING id"
+    values = [nok.name, nok.contact_number, nok.address, nok.postcode]
+    results = run_sql(sql, values)
+    nok.id = results[0]['id']
+    return nok
 
