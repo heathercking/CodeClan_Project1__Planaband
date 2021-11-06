2cc88378ba3f6b2ac2aa59ace3f5ef3e89569fae
Heather King <hkingdevelopment@gmail.com>
2021-11-06 14:35:05 +0000

    adds new_nok route to nok controller

--- controllers/nok_controller.py
+++ controllers/nok_controller.py
@@ -11,3 +11,24 @@ def noks():
     noks = nok_repository.select_all()
     return render_template("noks/index.html", all_noks=noks)
 
+
+# NEW - GET '/noks/new'
+@noks_blueprint.route("/noks/new", methods=['GET'])
+def new_nok():
+    noks = nok_repository.select_all()
+    return render_template("noks/new.html", all_noks=noks)
+
+
+#CREATE - POST '/noks'
+
+
+#SHOW - GET '/noks/<id>'
+
+
+#EDIT - GET '/noks/<id>/edit'
+
+
+#UPDATE - POST '/noks/<id>'
+
+
+#DELETE - '/noks/<id>'
