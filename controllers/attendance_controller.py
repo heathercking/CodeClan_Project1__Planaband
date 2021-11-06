b16d42ed5c608a6d009694368e188fbf6ca958c2
Heather King <hkingdevelopment@gmail.com>
2021-11-06 19:51:59 +0000

    adds attendances route and index.html file displaying table of all attendances

--- controllers/attendance_controller.py
+++ controllers/attendance_controller.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, request, redirect
 
 from flask import Blueprint
+from models.attendance import Attendance
 from models.lesson import Lesson
 from models.pupil import Pupil
 
+import repositories.attendance_repository as attendance_repository
 import repositories.lesson_repository as lesson_repository
 import repositories.pupil_repository as pupil_repository
 
 attendances_blueprint = Blueprint("attendances", __name__)
+
+
+@attendances_blueprint.route("/attendances")
+def attendances():
+    attendances = attendance_repository.select_all()
+    return render_template("attendances/index.html", all_attendances=attendances)
 
 
