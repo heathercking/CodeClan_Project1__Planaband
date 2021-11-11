4fa1c8875495ba9a33023381d3f601a019c4d4d0
Heather King <hkingdevelopment@gmail.com>
2021-11-11 12:06:42 +0000

    tidies up files, removes comments

--- db/planaband.sql
+++ db/planaband.sql
@@ -50,14 +50,3 @@ CREATE TABLE attendances (
     pupil_id INT REFERENCES pupils(id) ON DELETE CASCADE,
     attended BOOLEAN
 );
-
-
--- INSERT INTO tutors (name, contact_number, address, postcode) VALUES ('Rubeus Hagrid', '07999 777888', 'Hagrids Hut, Hogwarts School', 'EH42 2DD');
-
--- INSERT INTO noks (name, contact_number, address, postcode) VALUES ('Lily Potter', '07777 888999', 'The Potter Cottage, Godrics Hollow', 'EH53 9AZ');
-
--- INSERT INTO pupils (name, dob, instrument, grade, nok_id, notes) VALUES ('Harry Potter', DATE '2010-7-31', 'Piano', '4', 1, 'Sat grade 4 exam in April');
-
--- INSERT INTO lessons (name, date, instrument, group_status, tutor_id) VALUES ('Beginner Recorder', DATE '2021-11-27', 'Recorder', True, 1);
-
--- INSERT INTO attendances (lesson_id, pupil_id, attended) VALUES (1, 1, True);
